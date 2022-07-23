766835dd0c91185dad52ebd32d2a8b48a136a64b
AndrMshk <andrmshk1@gmail.com>
2022-07-23 19:33:51 +0300

    delete main component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
 import { MyRoutes } from '../common/routes/Routes';
-import Main from '../features/main/Main';
+import { NavLink } from 'react-router-dom';
 
 export const App = () => {
   return (
     <>
-      <Main />
       <MyRoutes />
+      <div className="App">
+        <nav>
+          <div>
+            <NavLink to="/login">Login</NavLink>
+          </div>
+          <div >
+            <NavLink to="/registration">Register</NavLink>
+          </div>
+          <div>
+            <NavLink to="/profile">Profile</NavLink>
+          </div>
+          <div >
+            <NavLink to="/error">404</NavLink>
+          </div>
+          <div >
+            <NavLink to="/password-recovery">Password recovery</NavLink>
+          </div>
+          <div >
+            <NavLink to="/new-password">New password</NavLink>
+          </div>
+          <div >
+            <NavLink to="/test">Test</NavLink>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
