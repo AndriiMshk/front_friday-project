5072c2eab477d8eca3f21e3039d4cdf44a150106
skov82 <skov82@ya.ru>
2022-07-25 20:45:30 +0300

    add signup thunk creator

--- src/features/registration/registration-reducer.ts
+++ src/features/registration/registration-reducer.ts
@@ -1,20 +1,26 @@
-const initialState = {};
+import {ThunkType} from '../../app/store';
+import {setAppStatusAC} from '../../app/app-reducer';
+import {AxiosError} from 'axios';
+import {commonError} from '../../utils/common-error';
+import {signupAPI} from './api';
 
-export type InitialStateType = typeof initialState
-
-export const registrationReducer = (
-  state: InitialStateType = initialState, action: RegistrationActionType): InitialStateType => {
-  switch (action.type) {
-    case 'SET-REGISTRATION':
-      return { ...state };
-    default:
-      return state;
-  }
+const initState = {};
+export const registrationReducer = (state: InitStateType = initState): InitStateType => {
+    return state;
 };
 
-export type RegistrationActionType =
-  | SetRegistrationACType
+export const signupTC = (email: string, password: string): ThunkType => {
+    return (dispatch) => {
+        dispatch(setAppStatusAC('loading'))
+        signupAPI.signup(email, password)
+            .then(res => {
+                console.log(res)
+                dispatch(setAppStatusAC('succeeded'))
+            })
+            .catch((error: AxiosError<{ error: string }>) => {
+                commonError(error, dispatch)
+            })
+    }
+}
 
-export type SetRegistrationACType = ReturnType<typeof setRegistrationAC>
-
-export const setRegistrationAC = () => ({ type: 'SET-REGISTRATION' } as const);
+export type InitStateType = typeof initState
