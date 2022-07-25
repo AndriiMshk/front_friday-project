abd9d2785d1eb390e3a21e0e4b0d1a4cdad77fd9
skov82 <skov82@ya.ru>
2022-07-25 23:36:49 +0300

    add react-hook-form + material-ui for confirmPassword field

--- src/features/registration/Registration.tsx
+++ src/features/registration/Registration.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import IconButton from '@mui/material/IconButton';
 import Input from "@material-ui/core/Input";
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
 import {useAppDispatch, useAppSelector} from '../../app/store';
 import {signupTC} from './registration-reducer';
-import {Navigate} from 'react-router-dom';
+import {Link, Navigate} from 'react-router-dom';
 import FormGroup from '@mui/material/FormGroup';
 import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export const Registration = () => {
     const dispatch = useAppDispatch()
     const isRegistered = useAppSelector(state => state.registration.isRegistered)
+    const [showPassword,setShowPassword] = useState<boolean>(false)
 
     const { control, handleSubmit } = useForm<FormData>()
 
@@ -40,18 +47,44 @@ export const Registration = () => {
                     defaultValue=''
                     render={({ field }) => <Input {...field} />}
                 />
+
                 <Controller
                     name='confirmPassword'
                     control={control}
                     defaultValue=''
-                    render={({ field }) => <Input {...field} />}
+                    render={({ field }) => (
+                        <FormControl sx={{ m: 1, width: '347px' }} variant="standard">
+                            <InputLabel htmlFor="confirm-password">Confirm password</InputLabel>
+                            <Input
+                                id="confirm-password"
+                                // type={values.showPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle visibility"
+                                            // onClick={handleClickShowPassword}
+                                            // onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {/*{values.showPassword ? <VisibilityOff /> : <Visibility />}*/}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                {...field}
+                            />
+                        </FormControl>
+                    )}
                 />
 
-                <Button type={'submit'} variant={'contained'} color={'primary'}>
+                <Button type={'submit'} variant={'contained'} color={'primary'}
+                        sx={{borderRadius: '30px', width: '347px', height: '36px'}}
+                >
                     Sign Up
                 </Button>
             </FormGroup>
         </form>
+
+        <p>Already have an account?</p>
+        <Link to='/login' > Sign In </Link>
     </>
 };
 
