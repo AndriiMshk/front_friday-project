fbbd7bb0bffb8b83c70121fc9bc6057b71e3c256
skov82 <skov82@ya.ru>
2022-07-25 22:11:20 +0300

    refactor registrationReducer

--- src/features/registration/registration-reducer.ts
+++ src/features/registration/registration-reducer.ts
@@ -4,17 +4,27 @@ import {AxiosError} from 'axios';
 import {commonError} from '../../utils/common-error';
 import {signupAPI} from './api';
 
-const initState = {};
-export const registrationReducer = (state: InitStateType = initState): InitStateType => {
-    return state;
-};
+const initState = {
+    isRegistered: false
+}
+export const registrationReducer = (
+    state: InitStateType = initState, action: ActionType
+): InitStateType => {
+    switch (action.type) {
+        case 'SET_REG':
+            return {...state, isRegistered: true}
+        default:
+            return state
+    }
+}
+const setRegistered = () => ({type: 'SET_REG'} as const)
 
 export const signupTC = (email: string, password: string): ThunkType => {
     return (dispatch) => {
         dispatch(setAppStatusAC('loading'))
         signupAPI.signup(email, password)
-            .then(res => {
-                console.log(res)
+            .then(() => {
+                dispatch(setRegistered())
                 dispatch(setAppStatusAC('succeeded'))
             })
             .catch((error: AxiosError<{ error: string }>) => {
@@ -24,3 +34,5 @@ export const signupTC = (email: string, password: string): ThunkType => {
 }
 
 export type InitStateType = typeof initState
+type SetRegType = ReturnType<typeof setRegistered>
+type ActionType = SetRegType
