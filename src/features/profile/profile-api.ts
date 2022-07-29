2b20311db9631b43c11cc5be31c6597297162705
AndrMshk <andrmshk1@gmail.com>
2022-07-29 17:48:43 +0300

    change baseURL for deploy

--- src/features/profile/profile-api.ts
+++ src/features/profile/profile-api.ts
@@ -2,14 +2,14 @@ import axios, { AxiosResponse } from 'axios';
 import { UserType } from './profile-reducer';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:7542/2.0/',
-  // baseURL: 'https://neko-back.herokuapp.com/2.0/',
+  // baseURL: 'http://localhost:7542/2.0/',
+  baseURL: 'https://neko-back.herokuapp.com/2.0/',
   withCredentials: true,
 });
 
 export const profileApi = {
   setNewUserName(name: string) {
     return instance.put<{ name: string },
-      AxiosResponse<{ updatedUser: UserType, error?: string }>>('/auth/me', { name });
+      AxiosResponse<{ updatedUser: UserType, error?: string }>>('auth/me', { name });
   },
 };
