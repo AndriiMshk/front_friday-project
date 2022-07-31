d643b86ee3b696e648adb5cef3c17b2059e163f8
skov82 <skov82@ya.ru>
2022-07-31 23:59:48 +0300

    add thunks for recovery and change password

--- src/features/auth/auth-reducer.ts
+++ src/features/auth/auth-reducer.ts
@@ -53,21 +53,42 @@ export const logoutTC = (): ThunkType => (dispatch) => {
         })
 }
 
-export type InitialStateType = typeof initialState
-export const signupTC = (email: string, password: string): ThunkType => {
-    return async (dispatch) => {
-        dispatch(setAppStatusAC('loading'))
-        try {
-            const data = await authAPI.signup(email, password)
-            dispatch(setAppStatusAC('succeeded'))
-            return data
-        } catch (e) {
-            commonError(e, dispatch)
-        }
+export const signupTC = (email: string, password: string): ThunkType => async dispatch => {
+    dispatch(setAppStatusAC('loading'))
+    try {
+        const data = await authAPI.signup(email, password)
+        dispatch(setAppStatusAC('succeeded'))
+        return data
+    } catch (e) {
+        commonError(e, dispatch)
+    }
+}
+
+export const sendEmail = (email: string): ThunkType => async dispatch => {
+    dispatch(setAppStatusAC('loading'))
+    try {
+        const res = await authAPI.forgot({
+            email,
+            from: 'Memo Cards Team <memocards@gmail.com>',
+            message: 'Test mail for password recovery'
+        })
+        dispatch(setAppStatusAC('succeeded'))
+    } catch (e) {
+        commonError(e, dispatch)
+    }
+}
+
+export const setNewPassword = (password: string, resetPasswordToken: string): ThunkType => async dispatch => {
+    dispatch(setAppStatusAC('loading'))
+    try {
+        const res = await authAPI.newPassword({password, resetPasswordToken});
+        dispatch(setAppStatusAC('succeeded'))
+    } catch (e) {
+        commonError(e, dispatch);
     }
 }
 
 //types
+export type InitialStateType = typeof initialState
 export type LoginActionType = SetIsLoggedInActionType
-
 export type SetIsLoggedInActionType = ReturnType<typeof setIsLoggedInAC>
