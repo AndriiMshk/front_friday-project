dc4425a3f2d61652f93c28d40f8eec81d26cb2f9
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-08-02 22:02:54 +0300

    css packs and cards tables

--- src/features/Cards/Cards.tsx
+++ src/features/Cards/Cards.tsx
@@ -1,82 +1,93 @@
-import React, { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../app/store';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useAppDispatch, useAppSelector} from '../../app/store';
+import {useNavigate, useParams} from 'react-router-dom';
 
-import { CardsTable } from './CardsTable';
-import { createCardTC, setCardsTC } from './cards-reducer';
-import { Button } from '@mui/material';
+import {CardsTable} from './CardsTable';
+import {createCardTC, setCardsTC} from './cards-reducer';
+import {Button} from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import useDebounce from '../../common/hooks/useDebounce';
+import style from "../Packs/Packs.module.css";
 
 export const Cards = () => {
 
-  const [question, setQuestion] = useState('');
+    const [question, setQuestion] = useState('');
 
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const userId = useAppSelector(state => state.profile._id);
-  const packUserId = useAppSelector(state => state.cards.packUserId);
-  const cards = useAppSelector(state => state.cards.cards);
-  const page = useAppSelector(state => state.cards.page);
-  const rowsPerPage = useAppSelector(state => state.cards.pageCount);
-  const pageCount = useAppSelector(state => state.cards.cardsTotalCount);
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const userId = useAppSelector(state => state.profile._id);
+    const packUserId = useAppSelector(state => state.cards.packUserId);
+    const cards = useAppSelector(state => state.cards.cards);
+    const page = useAppSelector(state => state.cards.page);
+    const rowsPerPage = useAppSelector(state => state.cards.pageCount);
+    const pageCount = useAppSelector(state => state.cards.cardsTotalCount);
 
-  const { packId } = useParams<'packId'>();
+    const {packId} = useParams<'packId'>();
 
-  const packName = useAppSelector(state =>
-    state.packs.cardPacks.find(el => el._id === packId)?.name);
+    const packName = useAppSelector(state =>
+        state.packs.cardPacks.find(el => el._id === packId)?.name);
 
 //временная заглушка на добавление карточки
-  const addNewCardHandler = () => {
-    const question = prompt('question');
-    const answer = prompt('answer');
-    if (packId && question && answer) {
-      dispatch(createCardTC({ cardsPack_id: packId, question, answer }));
-    }
-  };
+    const addNewCardHandler = () => {
+        const question = prompt('question');
+        const answer = prompt('answer');
+        if (packId && question && answer) {
+            dispatch(createCardTC({cardsPack_id: packId, question, answer}));
+        }
+    };
 
-  useEffect(() => {
-    if (packId) {
-      dispatch(setCardsTC(
-        {
-          cardsPack_id: packId,
-          page,
-          pageCount: rowsPerPage,
-          cardQuestion: !!question ? question : undefined,
-        }));
-    }
-  }, [packId, page, rowsPerPage, pageCount, cards.length, useDebounce(question)]);
+    useEffect(() => {
+        if (packId) {
+            dispatch(setCardsTC(
+                {
+                    cardsPack_id: packId,
+                    page,
+                    pageCount: rowsPerPage,
+                    cardQuestion: !!question ? question : undefined,
+                }));
+        }
+    }, [packId, page, rowsPerPage, pageCount, cards.length, useDebounce(question)]);
 
-  return (
-    <div>
-      {userId !== packUserId
-        ? <div><h3>{packName}</h3></div>
-        : <div>
-          <h3>{packName}</h3>
-          <Button
-            onClick={addNewCardHandler}
-            variant="contained"
-          >Add new card</Button>
-        </div>}
-      <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="search"
-          label="search"
-          variant="outlined"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-        />
-      </Box>
-      <CardsTable cards={cards} userId={userId} pageCount={pageCount} rowsPerPage={rowsPerPage} />
-    </div>
-  );
+    return (
+        <div className={style.wrapper}>
+            <div className={style.container}>
+                <div>
+                    {userId !== packUserId
+                        ? <div><h3>{packName}</h3></div>
+                        : <div>
+                            <h3>{packName}</h3>
+                            <div className={style.searchAndAdd}>
+                                <Box
+                                    component="form"
+                                    sx={{
+                                        '& > :not(style)': {m: 1, width: '25ch'},
+                                    }}
+                                    noValidate
+                                    autoComplete="off"
+                                >
+                                    <TextField
+                                        id="search"
+                                        label="search"
+                                        variant="outlined"
+                                        value={question}
+                                        onChange={(e) => setQuestion(e.target.value)}
+                                    />
+                                </Box>
+                                <Button
+                                    onClick={addNewCardHandler}
+                                    variant="contained"
+                                >Add new card</Button>
+                            </div>
+
+                        </div>}
+
+                    <div className={style.table}>
+                        <CardsTable cards={cards} userId={userId} pageCount={pageCount} rowsPerPage={rowsPerPage}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
--- src/features/Packs/Packs.tsx
+++ src/features/Packs/Packs.tsx
@@ -1,86 +1,141 @@
-import React, { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import { PacksTable } from './PacksTable';
-import { useAppDispatch, useAppSelector } from '../../app/store';
-import { createPackTC, setPacksTC } from './packs-reducer';
-import { FilterPanel } from './FilterPanel';
+import React, {useEffect, useState} from 'react';
+import {Navigate} from 'react-router-dom';
+import {PacksTable} from './PacksTable';
+import {useAppDispatch, useAppSelector} from '../../app/store';
+import {createPackTC, setPacksTC} from './packs-reducer';
 import useDebounce from '../../common/hooks/useDebounce';
-import { Button } from '@mui/material';
+import {Button} from '@mui/material';
+import style from './Packs.module.css'
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import {SliderFilter} from "./Slider";
 
 export const Packs = () => {
 
-  const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
 
-  const packs = useAppSelector(state => state.packs.cardPacks);
-  const userId = useAppSelector(state => state.profile._id);
+    const packs = useAppSelector(state => state.packs.cardPacks);
+    const userId = useAppSelector(state => state.profile._id);
 
-  const page = useAppSelector(state => state.packs.page);
-  const isLoggedIn = useAppSelector(state => state.login.isLoggedIn);
+    const page = useAppSelector(state => state.packs.page);
+    const isLoggedIn = useAppSelector(state => state.login.isLoggedIn);
 
-  const rowsPerPage = useAppSelector(state => state.packs.pageCount);
-  const pageCount = useAppSelector(state => state.packs.cardPacksTotalCount);
+    const rowsPerPage = useAppSelector(state => state.packs.pageCount);
+    const pageCount = useAppSelector(state => state.packs.cardPacksTotalCount);
 
-  const maxCardsCount = useAppSelector(state => state.packs.maxCardsCount);
-  const [filterByCardsCount, setFilterByCardsCount] = useState<number[]>([0, maxCardsCount]);
+    const maxCardsCount = useAppSelector(state => state.packs.maxCardsCount);
+    const [filterByCardsCount, setFilterByCardsCount] = useState<number[]>([0, maxCardsCount]);
 
-  const [isShowMyPacks, setIsShowMyPacks0] = useState(false);
+    const [isShowMyPacks, setIsShowMyPacks0] = useState(false);
 
-  const [packName, setPackName] = useState<string>('');
+    const [packName, setPackName] = useState<string>('');
 
-  const addNewPackHandler = () => {
-    const name = prompt();
-    if (name) {
-      dispatch(createPackTC(name));
-    }
-  };
+    const addNewPackHandler = () => {
+        const name = prompt();
+        if (name) {
+            dispatch(createPackTC(name));
+        }
+    };
 
-  useEffect(() => {
-    dispatch(setPacksTC(
-      {
+    useEffect(() => {
+        dispatch(setPacksTC(
+            {
+                page,
+                pageCount: rowsPerPage,
+                min: filterByCardsCount[0],
+                max: filterByCardsCount[1],
+                user_id: isShowMyPacks ? userId : undefined,
+                packName: !!packName ? packName : undefined,
+            }));
+    }, [
         page,
-        pageCount: rowsPerPage,
-        min: filterByCardsCount[0],
-        max: filterByCardsCount[1],
-        user_id: isShowMyPacks ? userId : undefined,
-        packName: !!packName ? packName : undefined,
-      }));
-  }, [
-    page,
-    pageCount,
-    rowsPerPage,
-    useDebounce(filterByCardsCount),
-    isShowMyPacks,
-    useDebounce(packName),
-    packs.length,
-  ]);
+        pageCount,
+        rowsPerPage,
+        useDebounce(filterByCardsCount),
+        isShowMyPacks,
+        useDebounce(packName),
+        packs.length,
+    ]);
 
-  if (!isLoggedIn) {
-    return <Navigate to={'/login'} />;
-  }
+    if (!isLoggedIn) {
+        return <Navigate to={'/login'}/>;
+    }
 
-  return (
-    <div>
-      <h3>Packs list</h3>
-      <Button
-        onClick={addNewPackHandler}
-        variant="contained"
-      >Add new pack</Button>
-      <FilterPanel
-        filterByCardsCount={filterByCardsCount}
-        setFilterByCardsCount={setFilterByCardsCount}
-        isShowMyPacks={isShowMyPacks}
-        setIsShowMyPacks={setIsShowMyPacks0}
-        packName={packName}
-        setPackName={setPackName}
-      />
-      <PacksTable
-        packs={packs}
-        userId={userId}
-        rowsPerPage={rowsPerPage}
-        pageCount={pageCount} />
-    </div>
-  );
-};
+    return (
+        <div className={style.wrapper}>
+            <div className={style.container}>
+                <div className={style.sidebar}>
+                    <div className={style.sidebarBlock}>
+                        <h2>Show packs</h2>
+                        <ButtonGroup disableElevation>
+                            <Button
+                                onClick={() => setIsShowMyPacks0(false)}
+                                variant={!isShowMyPacks ? 'contained' : 'text'}
+                            >All</Button>
+                            <Button
+                                onClick={() => setIsShowMyPacks0(true)}
+                                variant={isShowMyPacks ? 'contained' : 'text'}
+                            >My</Button>
+                        </ButtonGroup>
+                        <SliderFilter
+                            filterByCardsCount={filterByCardsCount}
+                            setFilterByCardsCount={setFilterByCardsCount}
+                        />
 
+                    </div>
+                </div>
+                <div className={style.mainBlock}>
+                    <h1 className={style.title}>Packs list</h1>
+                    <div className={style.searchAndAdd}>
+                        <Box
+                            component="form"
+                            sx={{
+                                '& > :not(style)': {m: 1, width: '25ch'},
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <TextField
+                                id="search"
+                                label="search"
+                                variant="outlined"
+                                value={packName}
+                                onChange={(e) => setPackName(e.target.value)}
+                            />
+                        </Box>
+                        <Button
+                            onClick={addNewPackHandler}
+                            variant="contained"
+                        >Add new pack</Button>
+                    </div>
+                    <div className={style.table}>
+                        <PacksTable
+                            packs={packs}
+                            userId={userId}
+                            rowsPerPage={rowsPerPage}
+                            pageCount={pageCount}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+/*
+
+                   {/!* <FilterPanel
+                        filterByCardsCount={filterByCardsCount}
+                        setFilterByCardsCount={setFilterByCardsCount}
+                        isShowMyPacks={isShowMyPacks}
+                        setIsShowMyPacks={setIsShowMyPacks0}
+                        packName={packName}
+                        setPackName={setPackName}
+                    />*!/}
+
+
+                );
+                };
+
+*/
 
 
