7e271712105a32daf9705fa1194f99f93ebbad07
skov82 <skov82@ya.ru>
2022-08-02 21:15:55 +0300

    add error page css

--- src/common/404-page/ErrorPage.tsx
+++ src/common/404-page/ErrorPage.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-
 export const ErrorPage = () => {
-    return <div>
-        <h2 style={{color: "red", textAlign: "center"}}
-        >Error 404: PAGE NOT FOUND
+    return <div className={''}>
+        <h2>
+            Error 404: PAGE NOT FOUND
         </h2>
     </div>
 }
