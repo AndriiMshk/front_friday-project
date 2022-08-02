b719041169833c6c65759e3ac938457510472d0e
skov82 <skov82@ya.ru>
2022-08-02 13:57:27 +0300

    add SignIn button to the header with link to /login, add styles

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -4,20 +4,36 @@ import testAva from '../../assets/avatar.png';
 import {Avatar} from '@mui/material';
 import {useAppSelector} from '../../app/store';
 import incubatorLogo from '../../assets/incubator.png'
+import Button from '@mui/material/Button';
+import {useNavigate} from 'react-router-dom';
 
 export const Header = () => {
 
+    const navigate = useNavigate()
     const profile = useAppSelector(state => state.profile);
     const isLoggedIn = useAppSelector(state => state.login.isLoggedIn);
 
-    return (
+    return <header>
         <div className={style.container}>
-            <div style={{width: '209px', height: '48px'}}><img src={incubatorLogo} alt="it-incubator"/></div>
-            {isLoggedIn && <div className={style.info}>
-                {profile.name}
-                <Avatar style={{height: '36px', width: '36px'}} alt="Remy Sharp"
-                        src={profile.avatar ? profile.avatar : testAva}/>
-            </div>}
+            <div style={{width: '209px', height: '48px'}}>
+                <img src={incubatorLogo} alt="it-incubator"/>
+            </div>
+
+            {isLoggedIn
+                ? <div className={style.wrapper}>
+                    {profile.name}
+                    <Avatar style={{height: '36px', width: '36px', marginLeft: '12px'}}
+                            alt="Remy Sharp"
+                            src={profile.avatar ? profile.avatar : testAva}
+                    />
+                  </div>
+                : <Button variant={'contained'}
+                          className={style.signIn}
+                          onClick={() => navigate('/login')}
+                  >
+                    Sign in
+                  </Button>
+            }
         </div>
-    );
+    </header>
 };
