aa2883b5bcb1a0f95933560dbdfdfb5b96a6f25c
AndrMshk <andrmshk1@gmail.com>
2022-09-07 19:16:39 +0300

    '1'

---
+++ src/features/profile/components/EditNameForm.tsx
@@ -0,0 +1,52 @@
+import { Button, FormControl, Input } from '@mui/material';
+import { InputAdornment } from '@material-ui/core';
+import * as React from 'react';
+
+export const EditNameForm: React.FC<EditNameFormPropsType> = (
+  { handleSubmit, handleChange, name, setEditMode, newName, error },
+) => {
+
+  const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    if (e.code === 'Escape') {
+      setEditMode(false);
+      name = newName;
+    }
+  };
+
+  return (
+    <FormControl>
+      <form onSubmit={handleSubmit}>
+        <Input
+          autoFocus
+          autoComplete={'off'}
+          onKeyDown={onKeyDownHandler}
+          name="name"
+          onChange={handleChange}
+          value={newName}
+          onBlur={() => handleSubmit()}
+          error={!!error}
+          endAdornment={
+            <InputAdornment position="end">
+              <Button
+                variant="contained"
+                style={{ height: '24px', width: '54px' }}
+                type={'submit'}
+                disabled={!!error}
+              >SAVE
+              </Button>
+            </InputAdornment>
+          }
+        />
+      </form>
+    </FormControl>
+  );
+};
+
+type EditNameFormPropsType = {
+  handleSubmit: () => void
+  handleChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+  name: string
+  newName: string
+  setEditMode: (mode: boolean) => void
+  error: string | undefined
+}
